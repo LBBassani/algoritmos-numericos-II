9fc80e84a90f75807721d8ece7dbf557250885b3
LBBassani <lorenabassani12@gmail.com>
2021-09-26 14:54:58 -0300

    Rodando segundo dia

--- 6Trabalho/Exercicio.m
+++ 6Trabalho/Exercicio.m
@@ -1,6 +1,49 @@
 function Exercicio()
-   diretorios = ["GMRES"; "Direto"];
-   direto = [false, true];
+  
+   diretorio = "GMRES"
+   
+   i = 1;
+   L = W = 1;     
+   n = m = n_m = 200;
+   a = c = 0;
+   b = L;
+   d = W;
+   
+   tic
+   [u,flag,relres,iter,resvec,x,y] = pvc2d(a,b,c,d,n,m, @(x,y,n,m) resfriador_bidimensional(x,y,n,m,i), false);
+   tempo = toc();
+ 
+   figure()
+   grafico_solucao(u,x,y,n,m)
+   hgsave (strjoin({"Figuras/"; diretorio;"/resfriador_"; int2str(n_m); "_tipo"; int2str(i); ".ofig"}, ""))
+   close()
+   save(strjoin({"Resultados/"; diretorio;"/resfriador_"; int2str(n_m); "_tipo"; int2str(i); ".mat"}, ""), "u","flag","relres","iter","resvec","x","y","n","m","tempo","diretorio");
+
+   I = [2,3];
+   N = [20,50,100,200];
+   for i = I
+     for n_m = N
+       n = m = n_m;
+       a = c = 0;
+       b = L;
+       d = W;
+       
+       tic
+       [u,flag,relres,iter,resvec,x,y] = pvc2d(a,b,c,d,n,m, @(x,y,n,m) resfriador_bidimensional(x,y,n,m,i), direto(q));
+       tempo = toc();
+     
+       figure()
+       grafico_solucao(u,x,y,n,m)
+       hgsave (strjoin({"Figuras/"; diretorio;"/resfriador_"; int2str(n_m); "_tipo"; int2str(i); ".ofig"}, ""))
+       close()
+       save(strjoin({"Resultados/"; diretorio;"/resfriador_"; int2str(n_m); "_tipo"; int2str(i); ".mat"}, ""), "u","flag","relres","iter","resvec","x","y","n","m","tempo","diretorio"); 
+
+     endfor
+   endfor
+   
+  
+   diretorios = ["Direto"];
+   direto = [true];
    for q = 1:length(direto)
        diretorio = strrep(diretorios(q, :), " ", "")
        I = [1,2,3];
